768492c4eeb36d1646befff7a4a53ea19b6133ad
Shavar Sean Cox <shavarsean@gmail.com>
2025-12-21 14:23:43 -0500

    revisions

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,10 +1,23 @@
+export interface JobExperience {
+  title: string;
+  company: string;
+  startDate: string;
+  endDate: string;
+  details: string[];
+}
+
 export interface NavigationLink {
   href: string;
   label: string;
   target?: string;
 }
 
+export interface SocialLink extends NavigationLink {
+  faIcon: string;
+}
+
 export interface NavigationData {
   header: NavigationLink[];
   footer: NavigationLink[];
+  socials: SocialLink[];
 }
